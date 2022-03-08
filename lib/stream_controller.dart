303a21207356f810f5be5e0cc6fb1fa489bac959
AzureAD\ThanhCao <thanh@espos.no>
2022-03-08 14:51:12 +0100

    useStreamController hook

---
+++ lib/stream_controller.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+const imgUrl = 'https://bit.ly/3x7J5Qt';
+
+class HomePage extends HookWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(
+      onListen: () {
+        controller.sink.add(0.0);
+      },
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: StreamBuilder<double>(
+        stream: controller.stream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const CircularProgressIndicator();
+          final rotation = snapshot.data ?? 0.0;
+          return GestureDetector(
+            onTap: (() {
+              controller.sink.add(rotation + 10.0);
+            }),
+            child: RotationTransition(
+              turns: AlwaysStoppedAnimation(rotation / 360.0),
+              child: Center(
+                child: Image.network(imgUrl),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
